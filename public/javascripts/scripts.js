7cc746e37d99303aab1218d778ca92f45a6280fa
aaron8912 <girardaaron89@gmail.com>
2024-03-26 15:32:08 -0400

    commit 15
    styling

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,9 @@
 function confirmDeletion() {
     return confirm("Are you are you want to delete this record?");
 }
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
